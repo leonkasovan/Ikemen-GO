c676a92e6e76f663ef02b3a432be008396275242
Novan@Laptop-BRI <dhani.novan@gmail.com>
2024-10-17 10:11:50 +0700

    fix build for gles

--- build/build.cmd
+++ build/build.cmd
@@ -17,4 +17,5 @@ echo Building Ikemen GO...
 echo. 
 
 rem go build -tags="gl,glfw" -trimpath -v -ldflags "-s -w -H=windowsgui" -o ./bin/Ikemen_GO_Batch.exe ./src
-go build -tags="gl,sdl" -trimpath -v -ldflags "-s -w -H=windowsgui" -o ./bin/Ikemen_GO_Batch.exe ./src
+rem go build -tags="gl,sdl" -trimpath -v -ldflags "-s -w -H=windowsgui" -o ./bin/Ikemen_GO_Batch.exe ./src
+go build -tags="gles,sdl" -trimpath -v -ldflags "-s -w -H=windowsgui" -o ./bin/Ikemen_GO_Batch.exe ./src
