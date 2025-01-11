6987b1334a54fefef9549e11505f5726fd407ba5
Novan(Steamdeck) <dhani.novan@steamdeck>
2025-01-11 17:11:10 +0700

    support mugen game

--- src/main.go
+++ src/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"archive/zip"
+	"bufio"
+	"bytes"
 	_ "embed" // Support for go:embed resources
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"regexp"
 	"runtime"
 	"strconv"
@@ -16,6 +21,404 @@ import (
 
 var Version = "development"
 var BuildTime = ""
+
+//go:embed assets.zip
+var assetsZip []byte
+
+//go:embed screenpack.zip
+var screenpackZip []byte
+
+// extractFileFromEmbed extracts a specific file from the embedded ZIP content by its name  into current dir.
+func extractFileFromEmbed(content []byte, filename string) error {
+	zipReader, err := zip.NewReader(bytes.NewReader(content), int64(len(content)))
+	if err != nil {
+		return err
+	}
+
+	// Search for the file in the archive
+	for _, file := range zipReader.File {
+		if file.Name == filename {
+			// Ensure the directory exists before creating the file
+			if err := os.MkdirAll(filepath.Dir(file.Name), os.ModePerm); err != nil {
+				return err
+			}
+
+			fileReader, err := file.Open()
+			if err != nil {
+				return err
+			}
+			defer fileReader.Close()
+
+			outFile, err := os.Create(file.Name)
+			if err != nil {
+				return err
+			}
+			defer outFile.Close()
+
+			_, err = io.Copy(outFile, fileReader)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+
+	return fmt.Errorf("file %s not found in archive", filename)
+}
+
+// extractEmbed extracts all files from the embedded ZIP content into current dir.
+func extractEmbed(content []byte) error {
+	// Open the embedded zip file from the byte slice
+	zipReader, err := zip.NewReader(bytes.NewReader(content), int64(len(content)))
+	if err != nil {
+		return err
+	}
+
+	// Iterate over the files in the zip archive
+	for _, file := range zipReader.File {
+		// fmt.Printf("Extracting: %s\n", file.Name)
+
+		// Open the file inside the zip archive
+		fileReader, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer fileReader.Close()
+
+		// Handle directories by creating them first
+		if file.FileInfo().IsDir() {
+			err := os.MkdirAll(file.Name, os.ModePerm)
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		// Ensure the directory exists before creating the file
+		if err := os.MkdirAll(filepath.Dir(file.Name), os.ModePerm); err != nil {
+			return err
+		}
+
+		// Create the destination file on disk
+		outFile, err := os.Create(file.Name)
+		if err != nil {
+			return err
+		}
+		defer outFile.Close()
+
+		// Copy the file contents to the destination file
+		_, err = io.Copy(outFile, fileReader)
+		if err != nil {
+			return err
+		}
+
+		// fmt.Printf("Successfully extracted: %s\n", file.Name)
+	}
+	return nil
+}
+
+// extractFile extracts a file from the ZIP archive to the specified path
+func extractFile(f *zip.File, filePath string) error {
+	// Open the file inside the ZIP archive
+	srcFile, err := f.Open()
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	// Create the destination file
+	destFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	// Copy the file content
+	_, err = io.Copy(destFile, srcFile)
+	return err
+}
+
+func stringInSlice(target string, slice []string) bool {
+	for _, str := range slice {
+		if str == target {
+			return true
+		}
+	}
+	return false
+}
+
+// Update Section [Characters] in select.def based on [char] directory
+func updateCharInSelectDef(fname string) error {
+	// Open the file
+	filename := fname
+	fmt.Printf("[main.go] fname=%v filename=%v\n", fname, filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	// Open or create the file
+	file2, err := os.Create(filename + ".update")
+	if err != nil {
+		file.Close()
+		return err
+	}
+
+	// Create a buffered writer
+	writer := bufio.NewWriter(file2)
+
+	// Create a new scanner
+	scanner := bufio.NewScanner(file)
+
+	// Loop through each line
+	var result []string
+	var line string
+	chars := make([]string, 0, 20)
+	section := 0
+	for scanner.Scan() {
+		line = strings.ToLower(scanner.Text())
+		if len(line) < 1 {
+			continue
+		}
+		if line[0] == ';' { // skip comment
+			writer.WriteString(scanner.Text() + "\n")
+			continue
+		}
+		if len(line) < 2 {
+			continue
+		}
+		if line[0] == ' ' && line[1] == ';' { // skip nested comment
+			writer.WriteString(scanner.Text() + "\n")
+			continue
+		}
+		if strings.Contains(line, "[characters]") {
+			section = 1
+			writer.WriteString(scanner.Text() + "\n")
+			continue
+		}
+		if strings.Contains(line, "[extrastages]") {
+			// Open the directory
+			files, err := os.ReadDir("chars")
+			if err != nil {
+				file.Close()
+				file2.Close()
+				return err
+			}
+
+			// List only directories
+			for _, file := range files {
+				if file.IsDir() {
+					if !stringInSlice(file.Name(), chars) {
+						fmt.Printf(" add new char: %v\n", file.Name())
+						writer.WriteString(file.Name() + ", random\n")
+					}
+				}
+			}
+			section = 2
+			writer.WriteString(scanner.Text() + "\n")
+			continue
+		}
+		if section == 1 {
+			result = regexp.MustCompile(`^(.+?),`).FindStringSubmatch(scanner.Text())
+			if result != nil {
+				writer.WriteString(scanner.Text() + "\n")
+				chars = append(chars, result[1])
+				fmt.Printf(" existing char: %v\n", result[1])
+				continue
+			}
+		}
+		writer.WriteString(scanner.Text() + "\n")
+	}
+	writer.Flush()
+	file.Close()
+	file2.Close()
+	err = os.Rename(filename, filename+".bak")
+	if err != nil {
+		fmt.Printf("[main.go] '%v' => '%v'\n\terr: %v\n", filename, filename+".bak", err)
+		return err
+	}
+	err = os.Rename(filename+".update", filename)
+	if err != nil {
+		fmt.Printf("[main.go] '%v' => '%v'\n\terr: %v\n", filename+".update", filename, err)
+		return err
+	}
+
+	return scanner.Err()
+}
+
+// Update Section [ExtraStages] in select.def based on files *.def in [stages] directory
+func updateStageInSelectDef(fname string) error {
+	path_sep1 := ""
+	path_sep2 := ""
+
+	// Open the file
+	filename := fname
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	// Open or create the file
+	file2, err := os.Create(filename + ".update")
+	if err != nil {
+		file.Close()
+		return err
+	}
+
+	// Create a buffered writer
+	writer := bufio.NewWriter(file2)
+
+	// Create a new scanner
+	scanner := bufio.NewScanner(file)
+
+	// Loop through each line
+	var line string
+	stages := make([]string, 0, 20)
+	section := 0
+	for scanner.Scan() {
+		line = strings.ToLower(scanner.Text())
+		if len(line) < 1 {
+			continue
+		}
+		if line[0] == ';' { // skip comment
+			writer.WriteString(scanner.Text() + "\n")
+			continue
+		}
+		if len(line) < 2 {
+			continue
+		}
+		if line[0] == ' ' && line[1] == ';' { // skip nested comment
+			writer.WriteString(scanner.Text() + "\n")
+			continue
+		}
+		if strings.Contains(line, "[characters]") {
+			section = 1
+			writer.WriteString(scanner.Text() + "\n")
+			continue
+		}
+		if strings.Contains(line, "[extrastages]") {
+			section = 2
+			writer.WriteString(scanner.Text() + "\n")
+			continue
+		}
+		if strings.Contains(line, "[options]") {
+			// Combine directory and pattern
+			searchPattern := filepath.Join("stages", "*.def")
+
+			// Get the list of files matching the pattern
+			files, err := filepath.Glob(searchPattern)
+			if err != nil {
+				file.Close()
+				file2.Close()
+				return err
+			}
+
+			// Print the matching files
+			for _, file := range files {
+				file = strings.Replace(file, path_sep2, path_sep1, -1)
+				if !stringInSlice(file, stages) {
+					fmt.Printf(" add new stage: %v\n", file)
+					writer.WriteString(file + "\n")
+				}
+			}
+			section = 3
+			writer.WriteString(scanner.Text() + "\n")
+			continue
+		}
+		if section == 2 {
+			writer.WriteString(scanner.Text() + "\n")
+			stages = append(stages, scanner.Text())
+			fmt.Printf(" existing stage: %v\n", scanner.Text())
+			if path_sep1 == "" {
+				if strings.Contains(scanner.Text(), "/") {
+					path_sep1 = "/"
+					path_sep2 = "\\"
+					// fmt.Printf("scanner.Text=%v path_sep1=%v path_sep2=%v\n", scanner.Text(), path_sep1, path_sep2)
+				}
+				if strings.Contains(scanner.Text(), "\\") {
+					path_sep1 = "\\"
+					path_sep2 = "/"
+				}
+			}
+			continue
+		}
+		writer.WriteString(scanner.Text() + "\n")
+	}
+	writer.Flush()
+	file.Close()
+	file2.Close()
+	os.Rename(filename, filename+".bak")
+	os.Rename(filename+".update", filename)
+	return scanner.Err()
+}
+
+// upgrade config.json from older version (below 0.98.x)
+func fixConfig(fname string) error {
+	// Open the file
+	filename := fname
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	// Open or create the file
+	file2, err := os.Create("save/config.fix.json")
+	if err != nil {
+		file.Close()
+		return err
+	}
+
+	// Create a buffered writer
+	writer := bufio.NewWriter(file2)
+
+	re1 := regexp.MustCompile(`"CommonAir": "(\S+)",`)
+	re2 := regexp.MustCompile(`"CommonCmd": "(\S+)",`)
+	re3 := regexp.MustCompile(`"CommonConst": "(\S+)",`)
+
+	// Create a new scanner
+	scanner := bufio.NewScanner(file)
+
+	// Loop through each line
+	var result []string
+	for scanner.Scan() {
+		result = re1.FindStringSubmatch(scanner.Text())
+		if result != nil {
+			writer.WriteString(fmt.Sprintf("\"CommonAir\": [\"%v\"],\n", result[1]))
+			continue
+		}
+		result = re2.FindStringSubmatch(scanner.Text())
+		if result != nil {
+			writer.WriteString(fmt.Sprintf("\"CommonCmd\": [\"%v\"],\n", result[1]))
+			continue
+		}
+		result = re3.FindStringSubmatch(scanner.Text())
+		if result != nil {
+			// fmt.Printf("[main.go][fixConfig]1: %v\n", scanner.Text())
+			// fmt.Printf("[main.go][fixConfig]2: %v\n", fmt.Sprintf("\"CommonConst\": [\"%v\"],\n", result[1]))
+			writer.WriteString(fmt.Sprintf("\"CommonConst\": [\"%v\"],\n", result[1]))
+			continue
+		}
+		// if strings.Contains(scanner.Text(), "external/shaders/") {
+		// 	writer.WriteString(fmt.Sprintf("\n"))
+		// 	continue
+		// }
+		// if strings.Contains(scanner.Text(), "MSAA") {
+		// 	writer.WriteString(fmt.Sprintf("  \"MSAA\": false,\n"))
+		// 	continue
+		// }
+		// if strings.Contains(scanner.Text(), "PostProcessingShader") {
+		// 	writer.WriteString(fmt.Sprintf("  \"PostProcessingShader\": 0,\n"))
+		// 	continue
+		// }
+		writer.WriteString(scanner.Text() + "\n")
+	}
+	writer.Flush()
+	file.Close()
+	file2.Close()
+	os.Rename(filename, "save/config.bak.json")
+	os.Rename("save/config.fix.json", filename)
+	return scanner.Err()
+}
 
 func init() {
 	runtime.LockOSThread()
@@ -49,11 +452,37 @@ func closeLog(f *os.File) {
 }
 
 func main() {
+	is_mugen_game := false
+	fmt.Printf("[main.go][main] Running at OS=[%v] ARCH=[%v]\n", runtime.GOOS, runtime.GOARCH)
+
+	// Check if the "external" directory exists and data/mugen.cfg, if not exists then extract assets from embedded
+	_, err1 := os.Stat("external")
+	_, err2 := os.Stat("data/mugen.cfg")
+	if os.IsNotExist(err1) && err2 == nil {
+		err := extractEmbed(assetsZip)
+		if err != nil {
+			fmt.Printf("[main.go][setupConfig] Error extracting asset: %v\n", err)
+		}
+		fmt.Println("[main.go][main] Mugen Game detected. Assets extraction completed successfully.")
+		is_mugen_game = true
+	}
+
+	processCommandLine()
+	if _, ok := sys.cmdFlags["-game"]; ok {
+		dir := filepath.Dir(sys.cmdFlags["-game"])
+		base := filepath.Base(sys.cmdFlags["-game"])
+		name := base[:len(base)-len(filepath.Ext(base))] // Remove the extension from the base name
+
+		err := os.Chdir(filepath.Join(dir, name))
+		if err != nil {
+			fmt.Println("Error changing directory:", err)
+			panic(err)
+		}
+	}
+
 	// Make save directories, if they don't exist
 	os.Mkdir("save", os.ModeSticky|0755)
 	os.Mkdir("save/replays", os.ModeSticky|0755)
-
-	processCommandLine()
 
 	// Try reading stats
 	if _, err := ioutil.ReadFile("save/stats.json"); err != nil {
@@ -65,7 +494,7 @@ func main() {
 	}
 
 	// Setup config values, and get a reference to the config object for the main script and window size
-	tmp := setupConfig()
+	tmp := setupConfig(is_mugen_game)
 
 	//os.Mkdir("debug", os.ModeSticky|0755)
 
@@ -284,7 +713,7 @@ type configSettings struct {
 var defaultConfig []byte
 
 // Sets default config settings, then attemps to load existing config from disk
-func setupConfig() configSettings {
+func setupConfig(is_mugen_game bool) configSettings {
 	// Unmarshal default config string into a struct
 	tmp := configSettings{}
 	chk(json.Unmarshal(defaultConfig, &tmp))
@@ -295,12 +724,29 @@ func setupConfig() configSettings {
 		cfgPath = sys.cmdFlags["-config"]
 	}
 	// Load the config file, overwriting the defaults
-	if bytes, err := ioutil.ReadFile(cfgPath); err == nil {
-		if len(bytes) >= 3 &&
-			bytes[0] == 0xef && bytes[1] == 0xbb && bytes[2] == 0xbf {
-			bytes = bytes[3:]
+	if FileExist(cfgPath) != "" {
+		counter := 0
+		for {
+			if bytes, err := os.ReadFile(cfgPath); err == nil {
+				if len(bytes) >= 3 &&
+					bytes[0] == 0xef && bytes[1] == 0xbb && bytes[2] == 0xbf {
+					bytes = bytes[3:]
+				}
+				if json.Unmarshal(bytes, &tmp) != nil {
+					fmt.Printf("[main.go] setupConfig fix %v\n", cfgPath)
+					if err := fixConfig(cfgPath); err != nil {
+						ShowErrorDialog(err.Error())
+						panic(err)
+					}
+				} else {
+					counter = 1
+				}
+				counter = counter + 1
+				if counter > 1 {
+					break
+				}
+			}
 		}
-		chkEX(json.Unmarshal(bytes, &tmp), "Error while loading the config file.\n")
 	}
 	// Fix incorrect settings (default values saved into config.json)
 	switch tmp.AudioSampleRate {
@@ -317,6 +763,55 @@ func setupConfig() configSettings {
 	tmp.PanningRange = ClampF(tmp.PanningRange, 0, 100)
 	tmp.Players = int(Clamp(int32(tmp.Players), 1, int32(MaxSimul)*2))
 	tmp.WavChannels = Clamp(tmp.WavChannels, 1, 256)
+
+	//Import Mugen setting
+	if is_mugen_game {
+		fmt.Printf("[main.go][setupConfig] import data/mugen.cfg\n")
+		file, err := os.Open("data/mugen.cfg")
+		if err != nil {
+			fmt.Printf("[main.go][setupConfig] Error loading data/mugen.cfg\n")
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		var result []string
+		var line string
+		for scanner.Scan() {
+			line = scanner.Text()
+			if len(line) < 1 {
+				continue
+			}
+			if line[0] == ';' {
+				continue
+			}
+			result = regexp.MustCompile(`[Mm]otif\s*=\s*(\S+)`).FindStringSubmatch(line)
+			if result != nil {
+				tmp.Motif = strings.ReplaceAll(result[1], "\\", "/")
+				fmt.Printf("[main.go][setupConfig] Import Motif=%v\n", tmp.Motif)
+				continue
+			}
+			result = regexp.MustCompile(`[Ss]tart[Ss]tage\s*=\s*(\S+)`).FindStringSubmatch(line)
+			if result != nil {
+				tmp.StartStage = strings.ReplaceAll(result[1], "\\", "/")
+				fmt.Printf("[main.go][setupConfig] Import StartStage=%v\n", tmp.StartStage)
+				continue
+			}
+			result = regexp.MustCompile(`[Gg]ame[Ww]idth\s*=\s*(\d+)`).FindStringSubmatch(line)
+			if result != nil {
+				tmp.GameWidth = int32(Atoi(result[1]))
+				fmt.Printf("[main.go][setupConfig] Import GameWidth=%v\n", tmp.GameWidth)
+				continue
+			}
+			result = regexp.MustCompile(`[Gg]ame[Hh]eight\s*=\s*(\d+)`).FindStringSubmatch(line)
+			if result != nil {
+				tmp.GameHeight = int32(Atoi(result[1]))
+				fmt.Printf("[main.go][setupConfig] Import GameHeight=%v\n", tmp.GameHeight)
+				continue
+			}
+		}
+	} else {
+		fmt.Printf("[main.go][setupConfig] NOT importing data/mugen.cfg\n")
+	}
 	// Save config file, indent with two spaces to match calls to json.encode() in the Lua code
 	cfg, _ := json.MarshalIndent(tmp, "", "  ")
 	chk(ioutil.WriteFile(cfgPath, cfg, 0644))
@@ -416,6 +911,73 @@ func setupConfig() configSettings {
 				Atoi(b[9].(string)), Atoi(b[10].(string)), Atoi(b[11].(string)),
 				Atoi(b[12].(string)), Atoi(b[13].(string))})
 		}
+	}
+
+	if _, ok := sys.cmdFlags["-updatechar"]; ok {
+		fmt.Printf("[main.go][setupConfig] Update data/select.def based on [char] directory\n")
+		err := updateCharInSelectDef("data/select.def")
+		if err != nil {
+			fmt.Printf("[main.go][setupConfig] %v\n", err)
+		}
+	}
+
+	if _, ok := sys.cmdFlags["-updatestage"]; ok {
+		fmt.Printf("[main.go][setupConfig] Update data/select.def based on [stages] directory\n")
+		err := updateStageInSelectDef("data/select.def")
+		if err != nil {
+			fmt.Printf("[main.go][setupConfig] %v\n", err)
+		}
+	}
+
+	if _, ok := sys.cmdFlags["-installrun"]; ok {
+		fmt.Printf("[main.go][setupConfig] Install default screenpack\n")
+		err := extractEmbed(screenpackZip)
+		if err != nil {
+			fmt.Printf("[main.go][setupConfig] Error extracting screenpack: %v\n", err)
+		}
+		err = extractEmbed(assetsZip)
+		if err != nil {
+			fmt.Printf("[main.go][setupConfig] Error extracting asset: %v\n", err)
+		}
+	}
+
+	if _, ok := sys.cmdFlags["-install"]; ok {
+		fmt.Printf("[main.go][setupConfig] Install default screenpack\n")
+		err := extractEmbed(screenpackZip)
+		if err != nil {
+			fmt.Printf("[main.go][setupConfig] Error extracting screenpack: %v\n", err)
+		}
+		err = extractEmbed(assetsZip)
+		if err != nil {
+			fmt.Printf("[main.go][setupConfig] Error extracting asset: %v\n", err)
+		}
+		os.Exit(0)
+	}
+
+	if _, ok := sys.cmdFlags["-audit"]; ok {
+		if FileExist("external/script/audit.lua") == "" {
+			err := extractFileFromEmbed(assetsZip, "external/script/audit.lua")
+			if err != nil {
+				fmt.Printf("[main.go][setupConfig] Error extracting audit.lua: %v\n", err)
+				os.Exit(0)
+			}
+		}
+		if FileExist("external/script/json.lua") == "" {
+			err := extractFileFromEmbed(assetsZip, "external/script/json.lua")
+			if err != nil {
+				fmt.Printf("[main.go][setupConfig] Error extracting json.lua: %v\n", err)
+				os.Exit(0)
+			}
+		}
+		l := lua.NewState()
+		l.Options.IncludeGoStackTrace = true
+		l.OpenLibs()
+		systemScriptInit(l)
+		fmt.Printf("\n\n==================================\nVerifying included the game assets...\n")
+		if err := l.DoFile("external/script/audit.lua"); err != nil {
+			fmt.Printf("[main.go][setupConfig] Error running audit script: %v\n", err)
+		}
+		os.Exit(0)
 	}
 
 	return tmp
