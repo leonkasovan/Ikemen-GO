3c3d9ec956599f6206bfb7933d1b448c814fff68
Novan(Raspberry Pi5) <leonkasovan@gmail.com>
2025-02-21 04:13:53 +0700

    update test.lua

--- external/script/test.lua
+++ external/script/test.lua
@@ -12,8 +12,9 @@ end
 
 function checkGroupParam(group, param, check_group_param)
 	for k, v in ipairs(check_group_param) do
-		if group:lower() == v[1] 
-		and v[2]:find(param:lower()) then
+		if group:lower() == v[1] and #v[2] > 0 and v[2]:find(param:lower()) then
+			return true
+		elseif group:lower() == v[1] and #v[2] == 0 then
 			return true
 		end
 	end
@@ -26,39 +27,87 @@ end
 -- local check_group_param = {{"common", ",air,cmd,const,states,"}, {"config", ",motif,windowicon,system,gamepadmappings,"}, {"debug",",font,"}}
 
 -- local filename = "chars/cage/cage.def"
--- local check_group_param = {{"files", ",cmd,cns,sprite,anim,sound,pal1,pal2,pa3,pal4,pal5,pal6,pal7,pal8,pal9,pal10,stcommon,st,st1,st2,st3,st4,st5,st6,st7,st8,st9"}}
+-- local check_group_param = {{"files", ""}}
 
-local filename = "stages/The_courtyard.def"
-local check_group_param = {{"music", ",bgmusic,"}, {"bgdef", ",spr,"}}
+-- local filename = "stages/The_courtyard.def"
+-- local check_group_param = {{"music", ",bgmusic,"}, {"bgdef", ",spr,"}}
+
+-- local filename = "data/select.def"
+-- local check_group_param = {{"characters", ""}, {"extrastages", ""}}
+
+-- local filename = "data/FIGHT.DEF"
+-- local check_group_param = {{"files", ""}}
+
+local filename = "data/System.def"
+local check_group_param = {
+	{"files", ""}, 
+	{"music", ",select.bgm,title.bgm,victory.bgm,"}, 
+	{"titlebgdef", ",spr,"}, 
+	{"selectbgdef", ",spr,"}, 
+	{"continue screen", ",bgm,"},
+	{"game over screen", ",storyboard,"},
+	{"default ending", ",storyboard,"},
+	{"end credits", ",storyboard,"}
+}
 
 content = f_fileRead(filename)
 if content == nil then
-	f_validation:writeln("[ERROR] Can not read "..filename)
+	print("[ERROR] Can not read "..filename)
 	return
 end
 
 local modified_line = ""
 local file, err = io.open(filename..".bak.txt", "w")
-for src_line in content:gmatch('([^\n]*)\n?') do
-    line = src_line:gsub('%s*;.*$', '')
-	if #line == 0 then goto skip end
-	if line:match('^[^%g]*%s*%[.-%s*%]%s*$') then --matched [] group
-		line = line:match('%[(.-)%s*%]%s*$')   --match text between []
-		line = line:gsub('[%. ]', '_')         --change . and space to _
-		group = tostring(line:lower())
-	else                                       --matched non [] line
-		local param, value = line:match('^%s*([^=]-)%s*=%s*(.-)%s*$')
-        if group ~= nil and param ~= nil and checkGroupParam(group, param, check_group_param) then
-			modified_line = param .. " = " .. value:gsub('\\','/')
-		end
-    end
 
-	::skip::
-	if #modified_line > 0 then
-		file:writeln(modified_line)
-	else
-		file:writeln(src_line)
+-- check it is select.def 
+if filename:lower():find("select%.def") then
+	for src_line in content:gmatch('([^\n]*)\n?') do
+		line = src_line:gsub('%s*;.*$', '')
+		if #line == 0 then goto skip end
+		if line:match('^[^%g]*%s*%[.-%s*%]%s*$') then --matched [] group
+			line = line:match('%[(.-)%s*%]%s*$')   --match text between []
+			group = tostring(line:lower())
+		else                                       --matched non [] line
+			if group ~= nil and checkGroupParam(group, "", check_group_param) then
+				modified_line = line:gsub('\\','/')
+			end
+		end
+
+		::skip::
+		if #modified_line > 0 then
+			file:writeln(modified_line)
+		else
+			file:writeln(src_line)
+		end
+		modified_line = ""
 	end
-	modified_line = ""
+else	-- it's other def file
+	for src_line in content:gmatch('([^\n]*)\n?') do
+	    line = src_line:gsub('%s*;.*$', '')
+		if #line == 0 then goto skip end
+		if line:match('^[^%g]*%s*%[.-%s*%]%s*$') then --matched [] group
+			line = line:match('%[(.-)%s*%]%s*$')   --match text between []
+			group = tostring(line:lower())
+		else                                       --matched non [] line
+			local param, value = line:match('^%s*([^=]-)%s*=%s*(.-)%s*$')
+	        if group ~= nil and param ~= nil and checkGroupParam(group, param, check_group_param) then
+				modified_line = param:lower() .. " = " .. value:gsub('\\','/')
+			end
+
+			if value ~= nil then
+				if value:find("%.") then
+					print("DEBUG", group, param, "value", value)
+				end
+			end
+	    end
+
+		::skip::
+		if #modified_line > 0 then
+			file:writeln(modified_line)
+		else
+			file:writeln(src_line)
+		end
+		modified_line = ""
+	end
 end
 file:close()
